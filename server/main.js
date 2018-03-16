a0a5388f23bc93c7f8e5004263d606c5b1b17457
marcofalsitta <marco.falsitta@gaitup.com>
2018-03-16 21:39:08 +0100

    master - adding python led comminication attempt

--- server/main.js
+++ server/main.js
@@ -5,6 +5,7 @@
  */
 'use strict';
 let PythonShell = require('python-shell');
+let cmd=require('node-cmd');
 
 const express = require('express');
 const app = express();
@@ -15,16 +16,35 @@ app.get('/', (req, res) =>{
 
 app.get('/control-led', (req, res)=>{
 
-  PythonShell.run('~/led.py', {}, (err)=>{
-    if (err) {
-      //throw err
-      res.json({msg:"could not complete because of an error", err:err});
-    }
-    else{
-      res.json({msg:"command successful"});
+  //PythonShell.run('~/led.py', {}, (err)=>{
+  //  if (err) {
+  //    //throw err
+  //    res.json({msg:"could not complete because of an error", err:err});
+  //  }
+  //  else{
+  //    res.json({msg:"command successful"});
+  //
+  //  }
+  //});
 
+  cmd.get(
+    'sudo python ~/led.py',
+    function(err, data, stderr){
+      if(err){
+        res.json({msg:"could not complete because of an error", err:err});
+      }
+      else{
+
+        console.log('data is : ',data);
+        console.log('stderr is : ',stderr);
+
+        res.json({msg:"command successful", data:data});
+
+      }
     }
-  });
+  );
+
+
 });
 
 app.listen(3000, ()=>{
