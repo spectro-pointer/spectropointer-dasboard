448c4a08658ed6d12cc9795ba6814f86bce9a1e9
marcofalsitta <marco.falsitta@gaitup.com>
2018-03-16 21:30:16 +0100

    master - adding python led comminication attempt

--- server/main.js
+++ server/main.js
@@ -4,12 +4,27 @@
  *
  */
 'use strict';
+let PythonShell = require('python-shell');
 
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
   res.send('Hello World!')
+});
+
+app.get('/control-led', (req, res)=>{
+
+  PythonShell.run('~/led.py', {}, (err)=>{
+    if (err) {
+      //throw err
+      res.json({msg:"could not complete because of an error", err:err});
+    }
+    else{
+      res.json({msg:"command successful"});
+
+    }
+  });
 });
 
 app.listen(3000, ()=>{
